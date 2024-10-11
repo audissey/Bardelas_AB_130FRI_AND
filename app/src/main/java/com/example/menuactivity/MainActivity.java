240d04edb930b1c1278e604724d7cab8196d4d94
Audisoy Bardelas <84054-22265615@SERVER2.DOMAIN>
2024-10-11 15:01:23 +0800

    FinalMenu

---
+++ app/src/main/java/com/example/menuactivity/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.menuactivity;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    if (item.getItemId() == R.id.nav_home) {
+                        selectedFragment = new HomeFragment();
+                    } else if (item.getItemId() == R.id.nav_dashboard) {
+                        selectedFragment = new DashboardFragment();
+                    } else if (item.getItemId() == R.id.nav_notifications) {
+                        selectedFragment = new NotificationsFragment();
+                    }
+
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, selectedFragment)
+                                .commit();
+                    }
+                    return true;
+                }
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+    }
+}
